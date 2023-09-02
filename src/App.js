0ed02de3251134b45640d6ff538edfb1d61c0f81
pigilambhargavi <pigilambhargavi@gmail.com>
2023-09-02 11:30:21 +0530

    yhuj

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Reviews from './Reviews';
 
 function App() {
   return (
     <div className="App">
-      <h1>welcome</h1>
+     <Reviews></Reviews>
 
 
     </div>
