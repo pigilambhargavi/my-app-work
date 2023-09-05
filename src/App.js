323d924308ceb8d10153d48032383f5d456fd659
pigilambhargavi <pigilambhargavi@gmail.com>
2023-09-05 11:39:35 +0530

    juyhgh

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import Shopping from './Shopping';
 function App() {
   return (
-    <div className="App">
-      <h1>welcome</h1>
-
-
+    <div>
+      <Shopping></Shopping>
     </div>
   );
 }
 
-export default App;
+export default App;
